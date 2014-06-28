ec3f7de29ab1f1f49392567d88919f89686f03c6
Eric Tran <erictran5791@gmail.com>
2014-06-28 03:22:59 -0400

    Added decimal functionality, updated UI.
    Added button icons, decimal functionality, and cleaned up the UI.
    Implemented a listener so that changing a spinner item will also update
    the output.

--- src/ca/erictran/weightconverter/MainActivity.java
+++ src/ca/erictran/weightconverter/MainActivity.java
@@ -3,6 +3,7 @@ package ca.erictran.weightconverter;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -10,22 +11,21 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.text.*;
 
-import android.R.string;
-import android.app.Activity;
-import android.view.inputmethod.*;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements OnItemSelectedListener {
 	
 	private String input;
 	private TextView output, output2;
+	private boolean decimalEntered, enteredAfterDecimal;
 	
 	private Spinner spinner, spinner2;
 
@@ -37,6 +37,8 @@ public class MainActivity extends ActionBarActivity {
         input = "0";
         output = (TextView) findViewById(R.id.textView2);
         output2 = (TextView) findViewById(R.id.textView3);
+        decimalEntered = false;
+        enteredAfterDecimal = false;
         
         spinner = (Spinner) findViewById(R.id.spinner1);
         spinner2 = (Spinner) findViewById(R.id.spinner2);
@@ -44,18 +46,13 @@ public class MainActivity extends ActionBarActivity {
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.units_array, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
         
         ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.units_array, android.R.layout.simple_spinner_item);
         adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner2.setAdapter(adapter2);
         spinner2.setSelection(1);
-    }
-    
-    //static method used to hide the keyboard
-    
-    public static void hideSoftKeyboard(Activity activity) {
-        InputMethodManager inputMethodManager = (InputMethodManager)  activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
-        inputMethodManager.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), 0);
+        spinner2.setOnItemSelectedListener(this);
     }
     
     public void onClick(View view) {
@@ -91,7 +88,19 @@ public class MainActivity extends ActionBarActivity {
     	case R.id.Button0:
     		input = input + "0";
     		break;
-    	case R.id.Button04:
+    	case R.id.ButtonDecimal:
+    		if (!decimalEntered) {
+    			decimalEntered = true;
+    			enteredAfterDecimal = false;
+    			input = input + ".";
+    		}
+    		break;
+    	case R.id.ButtonClear:
+    		input = "0";
+    		decimalEntered = false;
+    		enteredAfterDecimal = false;
+    		break;
+    	case R.id.ButtonBackspace:
         	if (input.length() == 0) {
         		Toast.makeText(this, "Please enter a number.", Toast.LENGTH_LONG).show();
         		return;
@@ -101,25 +110,51 @@ public class MainActivity extends ActionBarActivity {
     			input = "0";
     			return;
     		}
-    		else
+    		else {
+    			if (input.charAt(input.length() - 1) == '.') {
+    				decimalEntered = false;
+    				enteredAfterDecimal = false;
+    			}
     			input = input.substring(0, input.length()-1);
+    		}
     		break;
-    	case R.id.Button08:
+    	case R.id.ButtonSwitch:
     		int tempId = (int)spinner.getSelectedItemId();
     		spinner.setSelection((int)spinner2.getSelectedItemId());
     		spinner2.setSelection(tempId);
     		break;
     	}
     	
-    	double inputValue = Double.parseDouble(input);
-    	double inputValue2 = ConverterUtil.convertUnits(spinner.getSelectedItemId(), spinner2.getSelectedItemId(), inputValue);
+    	double inputValue;
+    	if (input.charAt(input.length() - 1) == '.') { //remove decimal from parse if it is trailing
+    		inputValue = Double.parseDouble(input.substring(0, input.length()-1));
+    	}
+    	else {
+    		inputValue = Double.parseDouble(input);
+    	}
+    	
+    	double outputValue = ConverterUtil.convertUnits(spinner.getSelectedItemId(), spinner2.getSelectedItemId(), inputValue);
     	DecimalFormat df = new DecimalFormat("#,###.##########");
     	
-    	output.setText(df.format(inputValue));
-		output2.setText(df.format(inputValue2));    	
+    	if (decimalEntered && !enteredAfterDecimal && input.charAt(input.length() - 1) != '.') { //if flag false and last char not a decimal
+    		enteredAfterDecimal = true;
+    	}
     	
-//    	output.setText(df.format(inputValue));
-//		output2.setText(df.format(ConverterUtil.convertUnits(spinner.getSelectedItemId(), spinner2.getSelectedItemId(), inputValue)));
+    	String regex = "^0+(?!$)"; //to remove leading zeros from input's output
+    	
+    	output.setText(input.replaceAll(regex, ""));
+    	output2.setText(df.format(outputValue));  
+    	
     }
+
+	@Override
+	public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+		onClick(view);
+	}
+
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {
+		
+	}
 
 }
