24ef4adffd638307e3f34eee727433a2d7b0aa78
Eric Tran <erictran5791@gmail.com>
2014-08-03 14:18:27 -0400

    Updated appearance, added screenshots.
    Cleaned up the colours and the UI. Added a screenshots folder.

--- src/ca/erictran/lengthweightconverter/TabListener.java
+++ src/ca/erictran/lengthweightconverter/TabListener.java
@@ -1,7 +1,6 @@
 package ca.erictran.lengthweightconverter;
 
 import android.support.v4.app.FragmentTransaction;
-import android.util.Log;
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.ActionBar.Tab;
 import com.actionbarsherlock.app.SherlockFragment;
@@ -19,14 +18,12 @@ public class TabListener implements ActionBar.TabListener {
 
     @Override
     public void onTabSelected(Tab tab, FragmentTransaction ft) {
-        Log.d("DEBUG", tab.getText()+" onTabSelected");
         ft.attach(fragment);
         ft.add(R.id.fragment_container, fragment);
     }
 
     @Override
     public void onTabUnselected(Tab tab, FragmentTransaction ft) {
-        Log.d("DEBUG", tab.getText()+" onTabUnselected");
         ft.detach(fragment);
     }
 
